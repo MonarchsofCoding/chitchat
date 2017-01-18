f9dd9af5cc7360ca08dc147e689b1f4b0f6f6ac6
VJ Patel <meetthevj@gmail.com>
2017-01-18 21:55:23 +0000

    Added initial Java Client with Gradle configuration

---
+++ java_client/ChitChatDesktop/src/com/moc/chitchat/Main.java
@@ -0,0 +1,15 @@
+package com.moc.chitchat;
+
+/**
+ *
+ */
+public class Main {
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("HELLO");
+    }
+}
