bd88e00b6e8981ea9ca615c223bb61b8bf068dd2
aakyo <aydinaakyol@gmail.com>
2017-01-26 14:01:54 +0000

    Deleted Instrument tests and added Unit Tests for Login And Registration Processes

---
+++ android_client/ChitChat/app/src/test/java/com/moc/chitchat/LoginTest.java
@@ -0,0 +1,82 @@
+package com.moc.chitchat;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created by aakyo on 23/01/2017.
+ */
+
+
+public class LoginTest {
+
+    private String usernameTyped;
+    private String passwordTyped;
+    private LoginController lController;
+
+    @Before
+    public void prep_controller() {
+        lController = Mockito.mock(LoginController.class, Mockito.CALLS_REAL_METHODS);
+    }
+
+    @Test
+    public void true_Input(){
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        usernameTyped = "aydinakyol";
+        passwordTyped = "Abc123!?";
+        lController.loginUser(usernameTyped,passwordTyped);
+
+        String expectedOutput  = "OK.\n";
+        assertEquals(expectedOutput, outContent.toString());
+    }
+
+    @Test
+    public void empty_UserInput() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        usernameTyped = "";
+        passwordTyped = "Abc123!?";
+        lController.loginUser(usernameTyped,passwordTyped);
+
+        String expectedOutput  = "ERROR: the username cannot be empty.\n";
+        assertEquals(outContent.toString(), expectedOutput);
+    }
+
+
+    @Test
+    public void empty_PassInput() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        usernameTyped = "aydinakyol";
+        passwordTyped = "";
+        lController.loginUser(usernameTyped,passwordTyped);
+
+        String expectedOutput  = "ERROR: the password cannot be empty.\n";
+        assertEquals(outContent.toString(), expectedOutput);
+    }
+
+
+    @Test
+    public void false_PassInput() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        usernameTyped = "aydinakyol";
+        passwordTyped = "Abc123";
+        lController.loginUser(usernameTyped,passwordTyped);
+
+        String expectedOutput  = "ERROR: the password does not match with the desired password" +
+            " pattern.\n";
+        assertEquals(outContent.toString(), expectedOutput);
+    }
+}
