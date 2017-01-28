127d23c8e262e824ac7bf06975f935f1f55442ea
VJ Patel <meetthevj@gmail.com>
2017-01-28 19:00:51 +0000

    Added Android reports and publishing them

--- android_client/tasks.py
+++ android_client/tasks.py
@@ -24,7 +24,7 @@ def test(ctx):
 
     lxc.Docker.run(cli,
         tag="{0}-dev".format("chitchat-androidclient"),
-        command='/bin/bash -c "cd app && gradle test"',
+        command='/bin/bash -c "cd app && gradle test && gradle jacocoTestReport && gradle lint"',
         volumes=[
             "{0}/ChitChat:/app".format(os.getcwd())
         ],
@@ -34,5 +34,29 @@ def test(ctx):
 
 @task
 def publish_test_artifacts(ctx):
-    # TODO
-    pass
+  cli.pull("google/cloud-sdk", "latest")
+
+  auth = "gcloud auth activate-service-account --key-file /terraform/travis-gcp-credentials.json"
+  gs_artifacts = "gs://kcl-chit-chat-artifacts/builds/{0}/android_client".format(os.getenv("TRAVIS_BUILD_NUMBER"))
+  acl = "gsutil -m acl ch -Ru AllUsers:R gs://kcl-chit-chat-artifacts"
+
+  local_coverage = "app/build/JacocoCoverageReport/jacocoTestReleaseUnitTestReport/html/*"
+  local_tests = "app/build/reports/tests/testReleaseUnitTest/release/*"
+  local_lint = "app/build/outputs/lint-results-debug.html"
+
+  lxc.Docker.run(cli,
+     tag="google/cloud-sdk",
+     command='/bin/sh -c "{0} && gsutil -m cp -r {1} {2}/coverage/ && gsutil -m cp -r {3} {2}/tests/ && gsutil -m cp -r {4} {2}/lint/index.html && {5}"'.format(
+        auth,
+        local_coverage,
+        gs_artifacts,
+        local_tests,
+        local_lint,
+        acl
+     ),
+     volumes=[
+       "{0}/ChitChat:/app".format(os.getcwd()),
+       "{0}/../backend/terraform/:/terraform".format(os.getcwd())
+     ],
+     working_dir="/app"
+   )
