6cac1923c6463b1535e31b7bf992faad33181090
Ozhan-Azizi <oazizi@hotmail.co.uk>
2017-02-01 19:03:18 +0000

    Adding Unit test for User model and removing setUsername function

---
+++ java_client/ChitChatDesktop/test/com/moc/chitchat/model/UserModelTest.java
@@ -0,0 +1,56 @@
+package com.moc.chitchat.model;
+
+import org.json.JSONObject;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Ozhan on 01/02/2017.
+ */
+public class UserModelTest {
+
+    @Test
+    public void testConstructor()
+    {
+        String expectedUsername = "Jacob";
+        UserModel userModel = new UserModel(expectedUsername);
+
+        assertEquals(userModel.getUsername(), expectedUsername);
+    }
+
+    @Test
+    public void testSetPassword()
+    {
+        String userName = "John";
+        UserModel userModel = new UserModel(userName);
+        String expectedPassword = "Cyber_Security";
+        userModel.setPassword(expectedPassword);
+
+        assertEquals(expectedPassword, userModel.getPassword());
+    }
+
+    @Test
+    public void testSetPasswordCheck()
+    {
+        String userName = "George";
+        UserModel userModel = new UserModel(userName);
+        String expectedPassword = "Cyber_Security";
+        userModel.setPasswordCheck(expectedPassword);
+
+        assertEquals(expectedPassword, userModel.getPasswordCheck());
+    }
+
+    @Test
+    public void testToJSONString()
+    {
+        String expectedUserName = "Ozhan";
+        UserModel userModel = new UserModel(expectedUserName);
+        String expectedPassword = "Security123";
+        userModel.setPassword(expectedPassword);
+
+        String expectedString = String.format("{\"password\":\"%s\",\"username\":\"%s\"}", expectedPassword, expectedUserName);
+        assertEquals(expectedString, userModel.toJSONString());
+
+    }
+
+}
