2fcdcef24bdec21129cf973e685e51aa78400c0e
VJ Patel <meetthevj@gmail.com>
2017-02-02 16:26:09 +0000

    [infra] Enabled DNS hostnames and DNS resolution to enable resolution for servicediscovery.internal

--- backend/terraform/aws/main.tf
+++ backend/terraform/aws/main.tf
@@ -138,6 +138,9 @@ data "aws_availability_zones" "available" {}
 
 resource "aws_vpc" "app" {
   cidr_block = "10.0.0.0/16"
+
+  enable_dns_support = true
+  enable_dns_hostnames = true
 }
 
 resource "aws_subnet" "app" {
