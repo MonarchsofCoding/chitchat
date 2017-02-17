670fd4a7ef8dc7358829696bfd9ff4d0932a53eb
VJ Patel <meetthevj@gmail.com>
2017-02-17 13:31:03 +0000

    [infrastructure] Fixed missing guardian key in terraform vars

--- backend/terraform/environments/beta/variables.tf
+++ backend/terraform/environments/beta/variables.tf
@@ -1,14 +1,19 @@
 variable "database_password" {
-  type = "string"
-  description = "The domain name for the Traefik ALB"
+  type        = "string"
+  description = "The database password for the application"
 }
 
 variable "secret_key_base" {
-  type = "string"
-  description = "The domain name for the Traefik ALB"
+  type        = "string"
+  description = "The secret key base for Phoenix Framework"
 }
 
 variable "container_version" {
-  type = "string"
-  description = "The domain name for the Traefik ALB"
+  type        = "string"
+  description = "The version of the container to deploy"
+}
+
+variable "guardian_secret_key" {
+  type        = "string"
+  description = "The secret key for Guardian JWT"
 }
