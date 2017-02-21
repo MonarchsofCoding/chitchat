c4e7ec17794551261d5f5d9fcc991a21848bc42f
Ozhan-Azizi <oazizi@hotmail.co.uk>
2017-02-21 16:31:57 +0000

    [java_client] Just adding getter and setters for Message class

---
+++ java_client/ChitChatDesktop/src/com/moc/chitchat/model/Message.java
@@ -0,0 +1,42 @@
+package com.moc.chitchat.model;
+
+/**
+ * Message class provides an individual message from a User to another User
+ */
+public class Message {
+
+    private UserModel from;
+    private UserModel to;
+    private String message;
+
+    public Message(UserModel from, UserModel to, String message) {
+        this.from = from;
+        this.to = to;
+        this.message = message;
+    }
+
+    public void setFrom(UserModel from) {
+        this.from = from;
+    }
+
+    public UserModel getFrom() {
+        return from;
+    }
+
+    public UserModel getTo() {
+        return to;
+    }
+
+    public void setTo(UserModel to) {
+        this.to = to;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
