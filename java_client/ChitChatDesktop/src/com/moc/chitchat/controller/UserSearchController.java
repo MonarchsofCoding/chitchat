6b262e327a6e15ff2cdafdb5c24e71b7034df71a
Ozhan-Azizi <oazizi@hotmail.co.uk>
2017-02-26 01:06:44 +0000

    [java_client] Clearing out the warnings for the controller and removed unused file

--- java_client/ChitChatDesktop/src/com/moc/chitchat/controller/UserSearchController.java
+++ java_client/ChitChatDesktop/src/com/moc/chitchat/controller/UserSearchController.java
@@ -9,18 +9,19 @@ import com.moc.chitchat.exception.ValidationException;
 import com.moc.chitchat.model.UserModel;
 import com.moc.chitchat.resolver.UserResolver;
 import com.moc.chitchat.validator.UserValidator;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.List;
-
 /**
- * UserSearchController is the controller for the user search feature
+ * UserSearchController is the controller for the user search feature.
  */
 @Component
 public class UserSearchController {
@@ -29,17 +30,31 @@ public class UserSearchController {
     private UserResolver userResolver;
     private UserValidator userValidator;
 
+    /**
+     * Constructor for the UserSearchController using the parameters below.
+     * @param httpClient - the http client
+     * @param userResolver - Resolver for the user
+     * @param userValidator - Validator for the user
+     */
     @Autowired
     public UserSearchController(
             HttpClient httpClient,
             UserResolver userResolver,
             UserValidator userValidator
-    ){
+    ) {
         this.httpClient = httpClient;
         this.userResolver = userResolver;
         this.userValidator = userValidator;
     }
 
+    /**
+     * searchUser function looks for the users using the parameter below.
+     * @param username - the name of te user
+     * @return - a list of the users that matches the string
+     * @throws UnirestException - if invalid get function that was passed
+     * @throws UnexpectedResponseException - unexpected response
+     * @throws ValidationException - If not enough characters inserted
+     */
     public List<UserModel> searchUser(String username)
             throws UnirestException, UnexpectedResponseException, ValidationException {
         Map<String, Object> mapper = new HashMap<>();
@@ -49,15 +64,14 @@ public class UserSearchController {
 
         if (response.getStatus() == 400) {
             this.userValidator.throwErrorsFromResponse(response);
-        }
-        else if (response.getStatus() != 200) {
+        } else if (response.getStatus() != 200) {
             throw new UnexpectedResponseException(response);
         }
 
         JSONArray jsonArray = response.getBody().getObject().getJSONArray("data");
         List<UserModel> foundUsers = new ArrayList<>();
 
-        for(Object obj: jsonArray){
+        for (Object obj : jsonArray) {
             JSONObject jsonObject = (JSONObject) obj;
             foundUsers.add(userResolver.getUserModelViaJSonObject(jsonObject));
         }
