4302408f684804b547cd8ae9e5ea861966d04b02
VJ Patel <meetthevj@gmail.com>
2017-03-01 17:38:00 +0000

    [backend] Back to only info logging with prod env

--- backend/chit_chat/config/prod.exs
+++ backend/chit_chat/config/prod.exs
@@ -22,7 +22,7 @@ config :chit_chat, ChitChat.Endpoint,
   # cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
-# config :logger, level: :info
+config :logger, level: :info
 
 # ## SSL Support
 #
