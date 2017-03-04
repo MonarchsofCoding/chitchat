877123d8643c24b2d066728ea2c69f9b0cc9fda6
VJ Patel <meetthevj@gmail.com>
2017-03-04 21:15:26 +0000

    [infra] Setting correct shell in dockerfile and updated entrypoint to try and get working on AWS ECS

--- backend/chit_chat/rel/entrypoint.sh
+++ backend/chit_chat/rel/entrypoint.sh
@@ -2,19 +2,20 @@
 
 set -e
 
-echo "# Setting LOCAL_IP"
+echo "Setting LOCAL_IP"
 export LOCAL_IP=$(hostname -i)
-echo "# Set LOCAL_IP as ${LOCAL_IP}"
+echo "Set LOCAL_IP as ${LOCAL_IP}"
 
 if [ -z ${ECS_DNS_POSTGRES+x} ]; then
   echo "Not on AWS ECS."
 else
+  echo "On AWS ECS"
   /opt/app/bin/chit_chat command Elixir.ChitChat.ReleaseTasks aws_ecs_dns
 
   source db_env
 fi
 
-echo "# Waiting for database to become available"
+echo "Waiting for database to become available"
 /bin/wait-for-it.sh -t 120 ${DATABASE_HOSTNAME}:${DATABASE_PORT}
 
 /opt/app/bin/chit_chat $@
