05d563af8b7105834c20dd28b34aa2b41a96ef8c
VJ Patel <meetthevj@gmail.com>
2017-03-06 14:35:44 +0000

    [backed] Credo and dogma linting

--- backend/chit_chat/web/presence/chitchat.ex
+++ backend/chit_chat/web/presence/chitchat.ex
@@ -1,4 +1,7 @@
 defmodule ChitChat.Presence do
+  @moduledoc """
+  provides presence for the pubsub.
+  """
   use Phoenix.Presence, otp_app: :chit_chat,
                         pubsub_server: ChitChat.PubSub
 end
