8fa939d94e76285de298e2fa06e8889325bdd9a8
Ozhan-Azizi <oazizi@hotmail.co.uk>
2017-03-07 11:53:07 +0000

    [java_client] UnexpectedResponse is changed to accept a Response object

--- java_client/ChitChatDesktop/src/com/moc/chitchat/exception/UnexpectedResponseException.java
+++ java_client/ChitChatDesktop/src/com/moc/chitchat/exception/UnexpectedResponseException.java
@@ -2,20 +2,21 @@ package com.moc.chitchat.exception;
 
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
+import okhttp3.Response;
 
 /**
  * UnexpectedResponseException is thrown when an unexpected status code occurs e.g. 500
  */
 public class UnexpectedResponseException extends Exception {
 
-    private HttpResponse<JsonNode> response;
+    private Response response;
 
-    public UnexpectedResponseException(HttpResponse<JsonNode> response) {
-        super(String.format("Unexpected Response code: %s", response.getStatus()));
+    public UnexpectedResponseException(Response response) {
+        super(String.format("Unexpected Response code: %s", response.code()));
         this.response = response;
     }
 
-    public HttpResponse<JsonNode> getResponse() {
+    public Response getResponse() {
         return this.response;
     }
 
