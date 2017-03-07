18467604e5c7106be04aa8aac74a3fb7ed91a09f
Ozhan-Azizi <oazizi@hotmail.co.uk>
2017-03-07 11:50:04 +0000

    [java_client] updating the messaging controller

--- java_client/ChitChatDesktop/src/com/moc/chitchat/controller/MessageController.java
+++ java_client/ChitChatDesktop/src/com/moc/chitchat/controller/MessageController.java
@@ -1,18 +1,17 @@
 package com.moc.chitchat.controller;
 
-import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import com.moc.chitchat.application.ChitChatData;
 import com.moc.chitchat.application.Configuration;
 import com.moc.chitchat.client.HttpClient;
 import com.moc.chitchat.exception.UnexpectedResponseException;
 import com.moc.chitchat.exception.ValidationException;
-import com.moc.chitchat.model.Conversation;
 import com.moc.chitchat.model.Message;
 import com.moc.chitchat.model.UserModel;
 import com.moc.chitchat.resolver.MessageResolver;
 import com.moc.chitchat.validator.UserValidator;
+import java.io.IOException;
+import okhttp3.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -54,20 +53,23 @@ public class MessageController {
      * @throws UnexpectedResponseException - unexpected response
      */
     public Message send(UserModel to, String message)
-            throws UnirestException, ValidationException, UnexpectedResponseException {
+            throws UnirestException, ValidationException, UnexpectedResponseException, IOException {
 
         Message newMessage = this.messageResolver.createMessage(this.configuration.getLoggedInUser(), to, message);
-        HttpResponse<JsonNode> response = httpClient.post("/api/v1/messages", newMessage);
+        Response response = httpClient.post("/api/v1/messages", newMessage);
 
-        if (response.getStatus() == 422) {
+
+        if (response.code() == 422) {
             this.userValidator.throwErrorsFromResponse(response);
-        } else if (response.getStatus() != 201) {
+        } else if (response.code() != 201) {
             throw new UnexpectedResponseException(response);
         }
 
         this.chitChatData.addMessageToConversation(to, newMessage);
 
+
         return newMessage;
+
     }
 
     /**
