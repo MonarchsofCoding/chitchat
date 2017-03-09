49be771669314503eaca5405ba7c81f5628689c5
Ozhan-Azizi <oazizi@hotmail.co.uk>
2017-03-09 02:38:50 +0000

    [java_client] Updating for the registration controller unit testing

--- java_client/ChitChatDesktop/test/com/moc/chitchat/controller/authentication/RegistrationControllerTest.java
+++ java_client/ChitChatDesktop/test/com/moc/chitchat/controller/authentication/RegistrationControllerTest.java
@@ -1,9 +1,11 @@
 package com.moc.chitchat.controller.authentication;
+
 import com.mashape.unirest.http.exceptions.UnirestException;
 import com.moc.chitchat.application.Configuration;
 import com.moc.chitchat.client.HttpClient;
 import com.moc.chitchat.exception.UnexpectedResponseException;
 import com.moc.chitchat.exception.ValidationException;
+import com.moc.chitchat.model.UserModel;
 import com.moc.chitchat.resolver.UserResolver;
 import com.moc.chitchat.validator.UserValidator;
 import okhttp3.HttpUrl;
@@ -66,9 +68,9 @@ public class RegistrationControllerTest {
                 userValidator,
                 httpClient
         );
-
+        UserModel userModel = new UserModel("testName");
         try {
-            registrationController.registerUser(
+            userModel = registrationController.registerUser(
                     validUsername,
                     validPassword,
                     validPasswordCheck
@@ -80,121 +82,215 @@ public class RegistrationControllerTest {
         }
 
         // assert requests
+        assertEquals("alice", userModel.getUsername().toString());
         RecordedRequest recordedRequest = server.takeRequest();
         assertEquals("//api/v1/users", recordedRequest.getPath());
         assertEquals("POST", recordedRequest.getMethod());
         server.shutdown();
     }
 
-/**
     @Test
-    public void testLocalUnsuccessfulRegisterUser() throws ValidationException, UnirestException, UnexpectedResponseException {
-        // Stub the UserResolver to return a UserModel
-        UserModel mockUser = mock(UserModel.class);
-        when(
-                this.mockUserResolver.createUser(
-                        "spiros",
-                        "aaa",
-                        "aaa"
-                )
-        ).thenReturn(mockUser);
+    public void testUnexpectedResponse()
+            throws UnirestException, IOException, InterruptedException {
+        String validUsername = "alice";
+        String validPassword = "abcde1234";
+        String validPasswordCheck = "abcde1234";
 
-        ValidationException mockValidationException = mock(ValidationException.class);
-        when(mockValidationException.getMessage())
-                .thenReturn("Validation Exception");
+        // Set up mock server
+        MockWebServer server = new MockWebServer();
 
-        doThrow(mockValidationException).when(this.mockUserValidator).validate(mockUser);
+        // Schedule the valid response
+        MockResponse mockResponse = new MockResponse();
 
+        // Not used, but for completeness
+        String jsonResponse = "{" +
+                "\"data\": {" +
+                "\"username\": \"alice\"" +
+                "}" +
+                "}";
 
-        // Run the function to test
+        mockResponse
+                .addHeader("Content-Type", "application/json")
+                .setBody(jsonResponse)
+                .setResponseCode(500)
+        ;
+
+        server.enqueue(mockResponse);
+
+        HttpUrl baseUrl = server.url("/");
+
+        // Set up controller
+        UserResolver userResolver = new UserResolver();
+        UserValidator userValidator = new UserValidator();
+        Configuration mockConfiguration = mock(Configuration.class);
+        when(mockConfiguration.getBackendAddress()).thenReturn(baseUrl.toString());
+        HttpClient httpClient = new HttpClient(mockConfiguration);
+
+        RegistrationController registrationController = new RegistrationController(
+                userResolver,
+                userValidator,
+                httpClient
+        );
+        UserModel userModel = new UserModel("testName");
         try {
-            this.registrationController.registerUser(
-                    "spiros",
-                    "aaa",
-                    "aaa"
+            userModel = registrationController.registerUser(
+                    validUsername,
+                    validPassword,
+                    validPasswordCheck
             );
-        } catch (ValidationException e) {
-            assertEquals("Validation Exception", e.getMessage());
-        }
-    }
 
-    @Test
-    public void testServerUnsuccessfulRegisterUser() throws UnirestException, ValidationException, UnexpectedResponseException {
-        // Stub the UserResolver to return a UserModel
-        UserModel mockUser = mock(UserModel.class);
-        when(
-                this.mockUserResolver.createUser(
-                        "spiros",
-                        "aaa",
-                        "aaa"
-                )
-        ).thenReturn(mockUser);
-
-
-        // Create and define the mocked response to return 422 (unsuccessful)
-        HttpResponse<JsonNode> mockResponse = (HttpResponse<JsonNode>) mock(HttpResponse.class);
-        when(mockResponse.getStatus())
-                .thenReturn(422);
-        // Stub the HTTPClient to return the mocked response
-        when(this.mockHttpClient.post("/api/v1/users", mockUser))
-                .thenReturn(mockResponse);
-
-        // Mock the ValidationException
-        ValidationException mockValidationException = mock(ValidationException.class);
-        when(mockValidationException.getMessage())
-                .thenReturn("Validation Exception");
-
-        // Run the function to test
-        try {
-            this.registrationController.registerUser(
-                    "spiros",
-                    "aaa",
-                    "aaa"
-            );
-        } catch (ValidationException e) {
-            assertEquals("Validation Exception", e.getMessage());
-        }
-
-        // Verify that UserValidator.validate was called
-        verify(mockUserValidator).validate(mockUser);
-        // Verify the UserValidator.throwErrorsFromResponse was called
-        verify(mockUserValidator).throwErrorsFromResponse(mockResponse);
-    }
-
-    @Test
-    public void testServerErrorRegisterUser() throws UnirestException, ValidationException {
-        // Stub the UserResolver to return a UserModel
-        UserModel mockUser = mock(UserModel.class);
-        when(
-                this.mockUserResolver.createUser(
-                        "spiros",
-                        "aaa",
-                        "aaa"
-                )
-        ).thenReturn(mockUser);
-
-
-        // Create and define the mocked response to return 422 (unsuccessful)
-        HttpResponse<JsonNode> mockResponse = (HttpResponse<JsonNode>) mock(HttpResponse.class);
-        when(mockResponse.getStatus())
-                .thenReturn(500);
-        // Stub the HTTPClient to return the mocked response
-        when(this.mockHttpClient.post("/api/v1/users", mockUser))
-                .thenReturn(mockResponse);
-
-        // Run the function to test
-        try {
-            this.registrationController.registerUser(
-                    "spiros",
-                    "aaa",
-                    "aaa"
-            );
+        } catch (ValidationException v) {
+            server.shutdown();
         } catch (UnexpectedResponseException e) {
-            assertEquals("Unexpected Response code: 500", e.getMessage());
+            assertEquals("testName", userModel.getUsername().toString());
+            server.shutdown();
         }
 
-        // Verify that UserValidator.validate was called
-        verify(mockUserValidator).validate(mockUser);
+        RecordedRequest recordedRequest = server.takeRequest();
+        assertEquals("//api/v1/users", recordedRequest.getPath());
+        assertEquals("POST", recordedRequest.getMethod());
+
+        server.shutdown();
     }
-*/
+
+    /**
+     * This test that if the server responds with 'should be at least 8 character(s)'
+     *
+     * @throws UnirestException
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    @Test
+    public void testValidationExceptionPassword()
+            throws UnirestException, IOException, InterruptedException {
+        String validUsername = "alice";
+        String validPassword = "12345";
+        String validPasswordCheck = "12345";
+
+        // Set up mock server
+        MockWebServer server = new MockWebServer();
+
+        // Schedule the valid response
+        MockResponse mockResponse = new MockResponse();
+
+        // Not used, but for completeness
+        String jsonResponse = "{" +
+                "\"errors\": {" +
+                "\"password\": [\"should be at least 8 character(s)\"]" +
+                "}" +
+                "}";
+
+        mockResponse
+                .addHeader("Content-Type", "application/json")
+                .setBody(jsonResponse)
+                .setResponseCode(422)
+        ;
+
+        server.enqueue(mockResponse);
+
+        HttpUrl baseUrl = server.url("/");
+
+        // Set up controller
+        UserResolver userResolver = new UserResolver();
+        UserValidator userValidator = new UserValidator();
+        Configuration mockConfiguration = mock(Configuration.class);
+        when(mockConfiguration.getBackendAddress()).thenReturn(baseUrl.toString());
+        HttpClient httpClient = new HttpClient(mockConfiguration);
+
+        RegistrationController registrationController = new RegistrationController(
+                userResolver,
+                userValidator,
+                httpClient
+        );
+        UserModel userModel = new UserModel("testName");
+        try {
+            userModel = registrationController.registerUser(
+                    validUsername,
+                    validPassword,
+                    validPasswordCheck
+            );
+
+        } catch (ValidationException v) {
+            v.printStackTrace();
+            server.shutdown();
+        } catch (UnexpectedResponseException e) {
+            e.printStackTrace();
+            server.shutdown();
+        }
+
+        RecordedRequest recordedRequest = server.takeRequest();
+        assertEquals("//api/v1/users", recordedRequest.getPath());
+        assertEquals("POST", recordedRequest.getMethod());
+        assertEquals("testName", userModel.getUsername().toString());
+        server.shutdown();
+    }
+
+    /**
+     * This tests the server error for a duplicated username
+     *
+     * @throws UnirestException
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    @Test
+    public void testValidationExceptionUsername()
+            throws UnirestException, IOException, InterruptedException {
+        String validUsername = "alice";
+        String validPassword = "12345678";
+        String validPasswordCheck = "12345678";
+
+        // Set up mock server
+        MockWebServer server = new MockWebServer();
+
+        // Schedule the valid response
+        MockResponse mockResponse = new MockResponse();
+
+        // Not used, but for completeness
+        String jsonResponse = "{" +
+                "\"errors\": {" +
+                "\"username\": [\"has already been taken\"]" +
+                "}" +
+                "}";
+
+        mockResponse
+                .addHeader("Content-Type", "application/json")
+                .setBody(jsonResponse)
+                .setResponseCode(422)
+        ;
+
+        server.enqueue(mockResponse);
+
+        HttpUrl baseUrl = server.url("/");
+
+        // Set up controller
+        UserResolver userResolver = new UserResolver();
+        UserValidator userValidator = new UserValidator();
+        Configuration mockConfiguration = mock(Configuration.class);
+        when(mockConfiguration.getBackendAddress()).thenReturn(baseUrl.toString());
+        HttpClient httpClient = new HttpClient(mockConfiguration);
+
+        RegistrationController registrationController = new RegistrationController(
+                userResolver,
+                userValidator,
+                httpClient
+        );
+        UserModel userModel = new UserModel("testName");
+        try {
+            userModel = registrationController.registerUser(
+                    validUsername,
+                    validPassword,
+                    validPasswordCheck
+            );
+        } catch (ValidationException v) {
+            server.shutdown();
+        } catch (UnexpectedResponseException e) {
+            e.printStackTrace();
+            server.shutdown();
+        }
+        RecordedRequest recordedRequest = server.takeRequest();
+        assertEquals("//api/v1/users", recordedRequest.getPath());
+        assertEquals("POST", recordedRequest.getMethod());
+        assertEquals("testName", userModel.getUsername().toString());
+        server.shutdown();
+    }
 }
