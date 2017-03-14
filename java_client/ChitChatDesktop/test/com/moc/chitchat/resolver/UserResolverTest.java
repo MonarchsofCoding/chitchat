b6f6058ad5dc0f300b4f909c3cf16b9d1b4d8cee
Ozhan-Azizi <oazizi@hotmail.co.uk>
2017-03-14 11:17:35 +0000

    [java_client] updating user resolver test due to crypto

--- java_client/ChitChatDesktop/test/com/moc/chitchat/resolver/UserResolverTest.java
+++ java_client/ChitChatDesktop/test/com/moc/chitchat/resolver/UserResolverTest.java
@@ -1,15 +1,18 @@
 package com.moc.chitchat.resolver;
 
+import com.moc.chitchat.crypto.CryptoFunctions;
 import com.moc.chitchat.model.UserModel;
 import org.json.JSONObject;
 import org.junit.Test;
 
+import java.security.KeyPair;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.spec.InvalidKeySpecException;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 /**
  * UserResolverTest provides tests for the UserResolver
@@ -43,27 +46,42 @@ public class UserResolverTest {
         assertEquals(expectedUsername, user.getUsername());
     }
 
-  /*  @Test
-    public void testCreateLoginUser()
-    {
+    @Test
+    public void testCreateLoginUser() throws Exception {
         String expectedUsename = "Vjftw";
         String expectedPasswordlgn = "aaaaaaaa";
 
+        CryptoFunctions cryptoFunctions = new CryptoFunctions();
+        KeyPair userKeyPair = cryptoFunctions.generateKeyPair();
+
         UserResolver userResolver = new UserResolver();
-        UserModel user = userResolver.createUser(expectedUsename,expectedPasswordlgn,expectedPublicKey,expectedPrivateKey);
+        UserModel user = userResolver
+                .createUser(expectedUsename,
+                            expectedPasswordlgn,
+                            userKeyPair.getPublic(),
+                            userKeyPair.getPrivate())
+                ;
 
         assertEquals(expectedUsename,user.getUsername());
         assertEquals(expectedPasswordlgn,user.getPassword());
+        assertEquals(userKeyPair.getPublic(), user.getPublicKey());
+        assertEquals(userKeyPair.getPrivate(), user.getPrivatekey());
     }
-*/
+
     @Test
-    public void testGetUserModelViaJSonObject() throws InvalidKeySpecException, NoSuchAlgorithmException {
+    public void testGetUserModelViaJSonObject() throws Exception {
+        CryptoFunctions cryptoFunctions = new CryptoFunctions();
+        KeyPair userKeyPair = cryptoFunctions.generateKeyPair();
+
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("username", "bob_dillion");
+        jsonObject.put("public_key", cryptoFunctions.keyToString(userKeyPair.getPublic()));
 
         UserResolver userResolver = new UserResolver();
         UserModel userModel = userResolver.getUserModelViaJSonObject(jsonObject);
 
         assertEquals("bob_dillion", userModel.getUsername());
+        assertEquals(userKeyPair.getPublic(), userModel.getPublicKey());
     }
+
 }
