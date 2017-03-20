9ff6c1ee3daeda68b618884f3fd33cfe60fb57d3
aakyo <aydinaakyol@gmail.com>
2017-03-20 14:40:52 +0000

    [android_client] Reverted back the CryptBox implementation

--- android_client/ChitChat/app/src/main/java/com/moc/chitchat/crypto/CryptoBox.java
+++ android_client/ChitChat/app/src/main/java/com/moc/chitchat/crypto/CryptoBox.java
@@ -1,6 +1,6 @@
 package com.moc.chitchat.crypto;
 
-import org.apache.commons.codec.binary.Base64;
+import android.util.Base64;
 
 import java.security.KeyFactory;
 import java.security.KeyPair;
@@ -25,8 +25,7 @@ public class CryptoBox {
     private static String keyGeneratorAlgorithm = "RSA";
 
     /**
-     * CryptoFunctions constructor.
-     * @throws NoSuchAlgorithmException No such Algorithm Security Exception if RSA doesn't exist.
+     * Constructor.
      */
     public CryptoBox() throws NoSuchAlgorithmException {
         this.generator = KeyPairGenerator.getInstance(CryptoBox.keyGeneratorAlgorithm);
@@ -34,44 +33,65 @@ public class CryptoBox {
     }
 
     /**
-     * In this function we generate the pair of keys using RSA and 4096 key lenght.
-     * @return the pair of the keys
-     * @throws Exception the exception if doesnt exist the RSA.
+     * Generates keys.
+     * @return The key pair.
+     * @throws Exception in case generation fails.
      */
-    public  KeyPair generateKeyPair() throws Exception {
-        this.generator.initialize(4096, new SecureRandom());
+    public KeyPair generateKeyPair() throws Exception {
+        generator.initialize(4096, new SecureRandom());
 
         return generator.generateKeyPair();
     }
 
     /**
-     * This function converts the string into a public key.
-     * @param pubKey the public key string as a parameter.
-     * @return the publicKey.
-     * @throws InvalidKeySpecException when there is an invalid key.
+     * Conversion from key to string.
+     * @param key The public key.
+     * @return The string form of the key.
+     */
+    public String keyToString(PublicKey key) {
+        String enc = Base64.encodeToString(key.getEncoded(), Base64.NO_WRAP);
+
+        System.out.println(enc);
+        return enc;
+    }
+
+    /**
+     * Conversion from key to string.
+     * @param key The private key.
+     * @return The string form of the key.
+     */
+    public String keyToString(PrivateKey key) {
+        return Base64.encodeToString(key.getEncoded(), Base64.NO_WRAP);
+    }
+
+    /**
+     * Returns the key object from string
+     * @param pubKey public key in string form
+     * @return The key object.
+     * @throws InvalidKeySpecException if the specification is invalid.
      */
     public PublicKey pubKeyStringToKey(String pubKey) throws InvalidKeySpecException {
-        X509EncodedKeySpec converterSpec = new X509EncodedKeySpec(Base64.decodeBase64(pubKey));
+        X509EncodedKeySpec converterSpec = new X509EncodedKeySpec(Base64.decode(pubKey, Base64.NO_WRAP));
         return keyFactory.generatePublic(converterSpec);
     }
 
     /**
-     * This function converts the string into a private key.
-     * @param privKey the private key string as a parameter.
-     * @return the private key
-     * @throws InvalidKeySpecException when there is an invalid key.
+     * Returns the key object from string
+     * @param privKey private key in string form
+     * @return The key object.
+     * @throws InvalidKeySpecException if the specification is invalid.
      */
     public PrivateKey privKeyStringToKey(String privKey) throws InvalidKeySpecException {
-        PKCS8EncodedKeySpec converterSpec = new PKCS8EncodedKeySpec(Base64.decodeBase64(privKey));
+        PKCS8EncodedKeySpec converterSpec = new PKCS8EncodedKeySpec(Base64.decode(privKey, Base64.NO_WRAP));
         return keyFactory.generatePrivate(converterSpec);
     }
 
     /**
-     * The encryption function help us to encrypt the messages that we want.
-     * @param plainText the message that we want to encrypt.
-     * @param publicKey the key we are going to use to encrypt.
-     * @return the ciphertext
-     * @throws Exception exception.
+     * Encryption.
+     * @param plainText The text to encrypt.
+     * @param publicKey The public key to encrypt the text.
+     * @return The cipher text
+     * @throws Exception if the encryption fails.
      */
     public String encrypt(String plainText, PublicKey publicKey) throws Exception {
         Cipher encryptCipher = Cipher.getInstance(CryptoBox.cipherAlgorithm);
@@ -79,40 +99,22 @@ public class CryptoBox {
 
         byte[] cipherText = encryptCipher.doFinal(plainText.getBytes("UTF-8"));
 
-        return Base64.encodeBase64String(cipherText);
+        return Base64.encodeToString(cipherText, Base64.NO_WRAP);
     }
 
     /**
-     * The decryption function decrypts the ciphertext and converts it into a plaintext.
-     * @param cipherText THe encrypted message that we want to decrypt.
-     * @param privateKey The private key that we will use to decrypt.
+     * Decryption.
+     * @param cipherText The cipherText to decipher.
+     * @param privateKey The private key to decipher the text.
      * @return The plaintext.
-     * @throws Exception throws Exception.
+     * @throws Exception if the decipher fails.
      */
     public String decrypt(String cipherText, PrivateKey privateKey) throws Exception {
-        byte[] bytes = Base64.decodeBase64(cipherText);
+        byte[] bytes = Base64.decode(cipherText, Base64.NO_WRAP);
 
         Cipher decriptCipher = Cipher.getInstance(CryptoBox.cipherAlgorithm);
         decriptCipher.init(Cipher.DECRYPT_MODE, privateKey);
 
         return new String(decriptCipher.doFinal(bytes), "UTF-8").trim();
-    }
-
-    /**
-     * This function converts the Public key in to a string in order to store it or tranfer it
-     * @param key the Pyblic key that we want to convert.
-     * @return The key into a String format.
-     */
-    public String keyToString(PublicKey key) {
-        return Base64.encodeBase64String(key.getEncoded());
-    }
-
-    /**
-     * This function converts the Private key into a string in order to store it or transfer it.
-     * @param key the Private Key that we want to convert.
-     * @return the key into a String format.
-     */
-    public String keyToString(PrivateKey key) {
-        return Base64.encodeBase64String(key.getEncoded());
     }
 }
