94e28608e92250f731e45637325c3c9c2a2e2e53
VJ Patel <meetthevj@gmail.com>
2017-03-21 11:21:33 +0000

    [backend] Fixed distributed gossip protocol not connecting hopefully

--- backend/chit_chat/config/prod.exs
+++ backend/chit_chat/config/prod.exs
@@ -26,8 +26,14 @@ config :logger, level: :info
 
 config :libcluster,
   topologies: [
-    gossip_example: [
+    gossip: [
       strategy: Elixir.Cluster.Strategy.Gossip,
+      config: [
+        port: 45892,
+        if_addr: {0,0,0,0},
+        multicast_addr: {230,1,1,251},
+        multicast_ttl: 2
+      ]
     ]
   ]
 
